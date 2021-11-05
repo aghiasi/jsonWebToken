6fb0a832644a8aef8d9b35e2ff03f1cb35b35897
aghiasi <ghisaikhamene@gmail.com>
2021-11-05 03:22:09 -0700

    git ignore the request

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,20 @@
 module.exports.singup_get =(req,res) => {
   res.render('signup')
 }
-module.exports.singup_post =(req,res) => {
-  res.send('new signup')
-}
 module.exports.login_get =(req,res) => {
   res.render('login')
 }
+module.exports.singup_post =(req,res) => {
+  const {email, password} = req.body;
+
+  console.log(email,password)
+
+  res.send('new signup')
+}
 module.exports.login_post =(req,res) => {
+  const {email, password} = req.body;
+
+  console.log(email,password)
+
  res.send('user login')
 }
